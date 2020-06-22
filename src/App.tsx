cc9d03995e1dbdc6af27c11d9752150a9b499d26
masvi <jeanmasvi@gmail.com>
2020-06-22 20:56:35 -0300

    eslint config -- error fixing

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className='App-header'>
         <h1>Test with react</h1>
       </header>
     </div>
