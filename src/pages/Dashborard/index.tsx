755d26371e6583fee65c57723187f6322dfd1645
masvi <jeanmasvi@gmail.com>
2020-06-30 07:01:36 -0300

    consuming github api with axios

--- src/pages/Dashborard/index.tsx
+++ src/pages/Dashborard/index.tsx
@@ -1,44 +1,65 @@
-import React from 'react';
-import logoImg from '../../assets/logo.svg';
-import { Title, Form, Repositories } from './styles';
+import React, { useState, FormEvent } from 'react';
+
 import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Repositories } from './styles';
+
+import logoImg from '../../assets/logo.svg';
+
+import api from '../../services/api';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>
+  ): Promise<void> {
+
+    event.preventDefault(); // previne o reload da pagina pelo evento do formulario
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="GitHub Explorer" />
       <Title>Explore respositórios no Github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="testes">
-          <img src="https://avatars0.githubusercontent.com/u/14143177?s=460&u=61d95e626cbcff279835d90e323fc12017694631&v=4" alt="Jean Masvi"></img>
-          <div>
-            <strong>teste/teste-repositorio</strong>
-            <p>apenas uma descrição de um repositorio ficticio</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="testes">
-          <img src="https://avatars0.githubusercontent.com/u/14143177?s=460&u=61d95e626cbcff279835d90e323fc12017694631&v=4" alt="Jean Masvi"></img>
-          <div>
-            <strong>teste/teste-repositorio</strong>
-            <p>apenas uma descrição de um repositorio ficticio</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="testes">
-          <img src="https://avatars0.githubusercontent.com/u/14143177?s=460&u=61d95e626cbcff279835d90e323fc12017694631&v=4" alt="Jean Masvi"></img>
-          <div>
-            <strong>teste/teste-repositorio</strong>
-            <p>apenas uma descrição de um repositorio ficticio</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="testes">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
 
     </>
