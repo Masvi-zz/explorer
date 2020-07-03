56c272050ee15639fb217a6665228e55f5a41736
masvi <jeanmasvi@gmail.com>
2020-07-02 22:19:32 -0300

    info styled-components to list projects

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,14 +1,52 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft } from 'react-icons/fi';
+import { Header, RepositoryInfo } from './styles';
+
+import logo from '../../assets/logo.svg';
 
 interface RepositoryParams {
   repository: string;
 }
 
 const Repository: React.FC = () => {
-  const { params } = useRouteMatch<RepositoryParams>();
+  //const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="Github Explorer" />
+        <Link to="/dashboard">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="" alt="" />
+          <div>
+            <strong>Rocketseat/unform</strong>
+            <p>teste teste </p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1090</strong>
+            <span>Start</span>
+          </li>
+          <li>
+            <strong>1090</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>1090</strong>
+            <span>Isuies</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+    </>
+  )
 };
 
 export default Repository;
